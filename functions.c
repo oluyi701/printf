4df6ca880f0d8e61ef0583ab4778f64a061aa4b8
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2022-08-09 16:15:32 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -12,7 +12,7 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_char(va_list types, char buffer[],
+nt print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
